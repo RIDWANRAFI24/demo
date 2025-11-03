6681f962ac428af9edf193a9403933001bf1c368
RIDWANRAFI24 <rzrafi119@gmail.com>
2025-11-03 23:53:50 +0600

    Replaced (2)concate.cpp with contents from (3)file.cpp

--- (2)concate.cpp
+++ (2)concate.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
+
 int main()
- {
- string first, last;// Lexical analysis (tokens)
- cout << "Enter your first name: ";// Syntax analysis
- cin >> first;
- cout << "Enter your last name: ";// Syntax analysis
- cin >> last;
- cout << "Full name: " << first + " " + last << endl;// Semantic analysis (checking similar or not)
+{
+    int n;  // Lexical analysis (token)
+    string name; // Lexical analysis (token)
+    float marks; // Lexical analysis (token)
+
+    cout << "Enter number of students: "; // Syntax analysis
+    cin >> n;
+
+    ofstream fout("students.txt");
+    for (int i = 0; i < n; i++)  // Syntax analysis (loop)
+    {
+        cout << "Enter name of student " << i + 1 << ": "; // Syntax analysis
+        cin >> name;
+        cout << "Enter marks of " << name << ": ";  // Syntax analysis
+        cin >> marks;
+        fout << name << " " << marks << endl; // Semantic analysis (file write)
+    }
+   fout.close(); // Semantic analysis (file close)
+
+    ifstream fin("students.txt"); // Semantic analysis (file checking)
+    cout << "\nStudent Records:\n";
+    while (fin>> name >> marks)
+    {
+        cout << "Name: " << name << ", Marks: " << marks << endl; // Semantic analysis (string & float output checking)
+    }
+    fin.close();      // Semantic analysis (file close)
 }
-
-
-
